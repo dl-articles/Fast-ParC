cbffa05a7eb4a4458a1ffeff80750e26105090d1
Vitaliy Vorobyov <vitalik.w555@gmail.com>
2022-12-07 14:16:23 +0300

    Deleted interpolation in fast-parc

---
+++ layers/fast_parc.py
@@ -0,0 +1,49 @@
+from collections.abc import Iterable
+
+import torch
+from torch import nn
+from torch.fft import fft, ifft
+
+
+class FastParCUnit(nn.Module):
+    def __init__(self, channels, dim, conv_dim='V'):
+        assert conv_dim in ['V', 'H'], "You can choice only vertical (V) or horizontal (H) dimension"
+        super().__init__()
+        self.dim = dim
+        self.channels = channels
+        self.weights = nn.Parameter(torch.rand((channels, dim)), requires_grad=True)
+        self.bias = nn.Parameter(torch.rand(channels), requires_grad=True)
+        self.conv_dim = conv_dim
+        self.conv_dim_idx = -2 if conv_dim == 'V' else -1
+
+    def process_weight_fft(self):
+        if self.conv_dim == 'V':
+            weights = self.weights.view(1, self.channels, 1, self.dim)
+            return torch.conj(fft(weights, dim=self.conv_dim_idx))
+        weights = self.weights.view(1, self.channels, self.dim, 1)
+        return torch.conj(fft(weights, dim=self.conv_dim_idx))
+
+    def forward(self, x):
+        x = fft(x, dim=self.conv_dim_idx)
+
+        x = x * self.process_weight_fft()
+
+        x = ifft(x, dim=self.conv_dim_idx).real
+
+        return x + self.bias.view(1, self.channels, 1, 1)
+
+
+class FastParC(nn.Module):
+
+    def __init__(self, channels, kernel):
+        super(FastParC, self).__init__()
+        v = h = kernel
+        if type(kernel) is tuple and len(kernel) == 2:
+            v, h = kernel
+        self.block = nn.Sequential(
+            FastParCUnit(channels, v),
+            FastParCUnit(channels, h, conv_dim="H")
+        )
+
+    def forward(self, x):
+        return self.block(x)
---
+++ main.py
@@ -0,0 +1,81 @@
+import torch
+from torch import nn
+
+from layers.fast_parc import FastParC
+
+
+class ConvModel(nn.Module):
+    def __init__(self):
+
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(1, 9, 3),
+            nn.Conv2d(9, 18, 3),
+            nn.Conv2d(18, 24, 3),
+            FastParC(24, 22),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(2904, 256),
+            nn.GELU(),
+            nn.Linear(256, 10)
+        )
+
+    def forward(self, x):
+        out = self.net(x)
+        out = out.view(out.size(0), -1)
+
+        return self.classifier(out)
+
+import os
+
+
+import torch
+from pytorch_lightning import LightningModule, Trainer
+from pytorch_lightning.callbacks.progress import TQDMProgressBar
+from pytorch_lightning.loggers import CSVLogger
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, random_split
+from torchmetrics import Accuracy
+from torchvision import transforms
+from torchvision.datasets import MNIST
+
+PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
+BATCH_SIZE = 256 if torch.cuda.is_available() else 64
+
+class MNISTModel(LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.l1 = ConvModel()
+
+    def forward(self, x):
+        return self.l1(x)
+
+    def training_step(self, batch, batch_nb):
+        x, y = batch
+        loss = F.cross_entropy(self(x), y)
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=1e-4)
+
+
+# Init our model
+mnist_model = MNISTModel()
+
+# Init DataLoader from MNIST Dataset
+train_ds = MNIST(PATH_DATASETS, train=True, download=True, transform=transforms.ToTensor())
+train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE)
+
+# Initialize a trainer
+trainer = Trainer(
+    accelerator="auto",
+    devices=1 if torch.cuda.is_available() else None,  # limiting got iPython runs
+    max_epochs=3,
+    callbacks=[TQDMProgressBar(refresh_rate=20)],
+)
+
+# Train the model ⚡
+trainer.fit(mnist_model, train_loader)
