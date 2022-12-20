009ae177fe2baeec214f0eda07f09c816b029edf
Vitaliy Vorobyov <vitalik.w555@gmail.com>
2022-12-20 16:18:21 +0300

    Added ability to restrict number of classes

--- dataset/ImageNetKaggle.py
+++ dataset/ImageNetKaggle.py
@@ -5,21 +5,28 @@ import json
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 class ImageNetKaggle(Dataset):
-    def __init__(self, root, split, transform=None):
+    def __init__(self, root, split, restrict_classes = 100, transform=None):
         self.samples = []
         self.targets = []
         self.transform = transform
         self.syn_to_class = {}
+        classes_count = 100
         with open(os.path.join(dir_path, "imagenet_class_index.json"), "rb") as f:
                     json_file = json.load(f)
                     for class_id, v in json_file.items():
+                        if 0 < restrict_classes < classes_count:
+                            return
                         self.syn_to_class[v[0]] = int(class_id)
+                        classes_count += 1
         with open(os.path.join(dir_path, "ILSVRC2012_val_labels.json"), "rb") as f:
                     self.val_to_syn = json.load(f)
         samples_dir = os.path.join(root, "ILSVRC/Data/CLS-LOC", split)
+
         for entry in os.listdir(samples_dir):
             if split == "train":
                 syn_id = entry
+                if syn_id not in self.syn_to_class:
+                    continue
                 target = self.syn_to_class[syn_id]
                 syn_folder = os.path.join(samples_dir, syn_id)
                 for sample in os.listdir(syn_folder):
@@ -28,6 +35,8 @@ class ImageNetKaggle(Dataset):
                     self.targets.append(target)
             elif split == "val":
                 syn_id = self.val_to_syn[entry]
+                if syn_id not in self.syn_to_class:
+                    continue
                 target = self.syn_to_class[syn_id]
                 sample_path = os.path.join(samples_dir, entry)
                 self.samples.append(sample_path)
